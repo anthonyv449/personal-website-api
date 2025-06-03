43f9db1241ac797ff633314bd94601810098f70f
Anthony Omar Valenzuela <anthonyv449@gmail.com>
2025-06-02 21:21:09 -0700

    update to get connection string

--- Program.cs
+++ Program.cs
@@ -24,7 +24,7 @@ var host = Host.CreateDefaultBuilder(args)
     .ConfigureServices((context, services) =>
     {
         // **Note**: no trailing space in the key name!
-        var conn = context.Configuration["PostgresConnection"];
+        var conn = context.Configuration.GetConnectionString("PostgresConnection");
         if (string.IsNullOrWhiteSpace(conn))
             throw new InvalidOperationException("PostgresConnection is not configured!");
 
